0921aeee3338fe616747778afb7f9e78f0d96d27
John Acera <john.acera@sportality.se>
2023-03-11 21:32:40 +0800

    initial commit with copy paste implementation from code reference in medium

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,41 +1,77 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <div>
+        <div v-if="isLoadingModel">Loading Model ...</div>
+        <div v-else>
+            <label for="avatar">Choose a picture:</label>
+            <input @change="getChange($event)" type="file" id="avatar" name="avatar" accept="image/*">
+        </div>
+        <img id="img_to_detect">
+        <canvas id="detect_result"></canvas>
+    </div>
 </template>
 
 <script>
+// import modules for object detection
+import '@tensorflow/tfjs-backend-cpu';
+import '@tensorflow/tfjs-backend-webgl';
+import * as cocoSsd from '@tensorflow-models/coco-ssd';
+import { onMounted, ref } from 'vue';
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
-  }
+  },
+  setup() {
+    const isLoadingModel = ref(false);
+    let cocoSsdModel = null;
+
+    onMounted(async () => {
+      isLoadingModel.value = true
+      isLoadingModel.value = false
+    });
+
+    const getChange = (e) => {
+        const url = URL.createObjectURL(e.target.files[0])
+        const img = document.getElementById('img_to_detect')
+        img.width = 500
+        img.height = 500
+        img.src = url
+        getDetect(img);
+    };
+
+    const getDetect = async (img) => {
+        // get detection result
+        cocoSsdModel = await cocoSsd.load();
+        const result = await cocoSsdModel.detect(img);
+        const canvas = document.getElementById('detect_result');
+        const color=["red","green","blue"]
+        canvas.width=img.width ;
+        canvas.height=img.height ;
+        const context = canvas.getContext('2d');
+        context.drawImage(img, 0, 0, img.width,img.height);
+        context.font = '40px Arial';
+
+        for (let i = 0; i < result.length; i++) {
+            context.beginPath();
+            context.rect(...result[i].bbox);
+            context.lineWidth = 5;
+            context.strokeStyle = color[i%3];
+            context.fillStyle = color[i%3];
+            context.font = "10px Verdana";
+            context.stroke();
+            context.fillText(
+                result[i].score.toFixed(3) + ' ' + result[i].class, 
+                result[i].bbox[0],
+                result[i].bbox[1] - 5);
+        }
+        return result
+    };
+
+    return {
+      getChange,
+      getDetect,
+    };
+  },
 }
 </script>
 
